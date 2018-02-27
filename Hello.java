402e35212e02658c0b37a5d86f4168ef66300623
akbar5 <36657017+akbar5@users.noreply.github.com>
2018-02-27 14:45:46 +0530

    KPMGDEM-4
    KPMGDEM-4

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello{
 
 for(int i=1;i<=10;i++){
 
-System.out.println("Hello DevOps Welcome to KPMG Happy Birthday to You by Akbar Jafri... " +i); 
+System.out.println("Welcome to DevOps Workshop... " +i); 
 
 
 }
